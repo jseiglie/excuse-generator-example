3ae19013718bddaf6fc993b250525731042f47ae
jseiglie <javierseiglie@gmail.com>
2023-03-20 20:03:09 +0000

    class example uploaded

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+//variables
+const excuse = document.querySelector("#excuse");
+const btn = document.querySelector("#button");
+let generatedExcuse = "";
+
+//code
+// data
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+const dataset = [who, action, what, when];
+
+function RandomNumber(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+function GetItem(arr, i) {
+  return arr[i];
+}
+function GetString(el) {
+  return `${GetItem(el, RandomNumber(el))} `;
+}
+
+function newExcuse(data) {
+  return (generatedExcuse = data.map(el => `${GetString(el)}`)).join("");
+}
+//console.log(GetItem(el, RandomNumber(el))));
+generatedExcuse = newExcuse(dataset);
+
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excuse.innerHTML = generatedExcuse;
 };
+
+btn.addEventListener("click", () => (excuse.innerHTML = newExcuse(dataset)));
